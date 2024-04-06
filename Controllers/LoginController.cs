08d2121e66646ef2fd4906b8b1836f6e25b959e6
kmlwoj <wujekkama@gmail.com>
2024-04-06 21:52:58 +0200

    Minor fix to the initial access token expiry

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -299,7 +299,7 @@ namespace lojalBackend.Controllers
             var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
               _configuration["Jwt:Audience"],
               claims,
-              expires: DateTime.Now.AddDays(-1),
+              expires: DateTime.Now.AddMinutes(30),
               signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
