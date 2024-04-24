6c992fb933b84fdeee0b58a4df70a3b56e1d9e10
kmlwoj <wujekkama@gmail.com>
2024-04-24 14:45:01 +0200

    Added method for getting current user data

---
+++ Models/UserDbModelOrg.cs
@@ -0,0 +1,13 @@
+﻿using DydaktykaBackend.Models;
+
+namespace lojalBackend.Models
+{
+    public class UserDbModelOrg : UserDbModel
+    {
+        public string Organization { get; set; }
+        public UserDbModelOrg(string login, string? email, AccountTypes type, int credits, DateTime? latestUpdate, string organization) : base(login, email, type, credits, latestUpdate)
+        {
+            Organization = organization;
+        }
+    }
+}
