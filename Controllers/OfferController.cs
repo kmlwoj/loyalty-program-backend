1551cb9a6d9e84a92380875eb28782219c141ea2
kmlwoj <wujekkama@gmail.com>
2024-05-05 13:10:21 +0200

    Added delete method for offers

--- Controllers/OfferController.cs
+++ Controllers/OfferController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using static lojalBackend.ImageManager;
 
 namespace lojalBackend.Controllers
 {
@@ -161,6 +162,46 @@ namespace lojalBackend.Controllers
                 await transaction.CommitAsync();
             }
             return Ok("Offer changed!");
+        }
+        /// <summary>
+        /// Deletes targeted offer
+        /// </summary>
+        /// <param name="ID">Targeted offer ID</param>
+        [Authorize(Policy = "IsLoggedIn", Roles = "Administrator")]
+        [HttpDelete("DeleteOffer/{ID:int}")]
+        public async Task<IActionResult> DeleteOffer(int ID)
+        {
+            DbContexts.ShopContext.Offer? checkOffer = await shopDbContext.Offers.FindAsync(ID);
+            if (checkOffer == null) 
+                return NotFound("Offer with the given ID was not found!");
+
+            using (var transaction = await shopDbContext.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    var discounts = await shopDbContext.Discounts.Where(x => x.OfferId == ID).ToListAsync();
+                    if(discounts != null)
+                        shopDbContext.Discounts.RemoveRange(discounts);
+
+                    var codes = await shopDbContext.Codes.Where(x => x.OfferId == ID).ToListAsync();
+                    if(codes != null)
+                        shopDbContext.Codes.RemoveRange(codes);
+
+                    shopDbContext.Offers.Remove(checkOffer);
+
+                    await shopDbContext.SaveChangesAsync();
+
+                    string fileName = string.Concat("Offers/", ID);
+                    DeleteFile(fileName);
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+                await transaction.CommitAsync();
+            }
+            return Ok("Offer deleted!");
         }
     }
 }
