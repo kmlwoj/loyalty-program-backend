1a9d536622704b45448146fe2af26a27397ee466
kmlwoj <wujekkama@gmail.com>
2024-05-09 20:41:30 +0200

    Updated MySQL scripts and readme

--- mysqlStartup.bat
+++ mysqlStartup.bat
@@ -1,6 +1,7 @@
 @echo off
 docker pull mysql
 docker volume create mysql-vol
+docker network create -d bridge loj-backend-network
 cls
 set str="%cd%\Scripts:/docker-entrypoint-initdb.d"
-docker run -itd --rm --name mysql -p 3306:3306 -v %str% --mount source=mysql-vol,target=/var/lib/mysql -e MYSQL_ROOT_PASSWORD=db_user_pass mysql:latest
+docker run -itd --rm --name mysql --network=loj-backend-network -p 3306:3306 -v %str% --mount source=mysql-vol,target=/var/lib/mysql -e MYSQL_ROOT_PASSWORD=db_user_pass mysql:latest
