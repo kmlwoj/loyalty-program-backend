7c26852c3ac03488f975fb18f6fb078abad833f4
putii <patrykol1964@gmail.com>
2024-05-26 19:49:11 +0200

    ContactInfoModel

---
+++ Models/ContactInfoModel.cs
@@ -0,0 +1,26 @@
+﻿namespace lojalBackend.Models
+{
+    public class ContactInfoModel
+    {
+        public string? Name { get; set; }
+        public string? Position { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public ContactInfoModel()
+        {
+            Name = null;
+            Position = string.Empty;
+            Email = string.Empty;
+            Phone = string.Empty;
+        }
+
+        public ContactInfoModel(string? name, string email, string phone, string? position)
+        {
+            Name = name;
+            Position = position;
+            Email = email;
+            Phone = phone;
+        }
+    }
+}
