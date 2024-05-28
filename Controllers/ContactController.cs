9ce7727f92eea777b600c71e056f84b487a7807a
putii <patrykol1964@gmail.com>
2024-05-28 12:34:23 +0200

    ContactControler::AddContact v1

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -41,7 +41,42 @@ namespace lojalBackend.Controllers
             return new JsonResult(answer);
         }
 
-
+        /// <summary>
+        /// Adds new contact to the system
+        /// </summary>
+        /// <returns>Success message</returns>
+        /// <param name="contact">New contact</param>
+        /// <response code="200">Contact added successfully</response>
+        /// <response code="400">Bad request</response>
+        [Authorize(Policy = "IsLoggedIn", Roles = "Administrator")]
+        [HttpPost("AddContact")]
+        public async Task<IActionResult> AddContact([FromBody] ContactInfoModel contact)
+        {
+            using (var clientTransaction = await clientDbContext.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    DbContexts.MainContext.ContactInfo newDbContextsContact = new()
+                    {
+                        Name = contact.Name,
+                        Email = contact.Email,
+                        Phone = contact.Phone,
+                        Position = contact.Position
+                    }; // it would be great to have a constructor in ContactInfo
+                       // that takes a ContactInfoModel object as a parameter and
+                       // initializes the properties with the values from the model
+                    await clientDbContext.ContactInfos.AddAsync(newDbContextsContact);
+                    await clientDbContext.SaveChangesAsync();
+                    await clientTransaction.CommitAsync();
+                    return Ok("Contact added successfully!"); // returns 200 OK response
+                }
+                catch (Exception e)
+                {
+                    await clientTransaction.RollbackAsync();
+                    return BadRequest(e.Message); // returns 400 Bad Request response
+                }
+            }
+        }
 
     }
 }
