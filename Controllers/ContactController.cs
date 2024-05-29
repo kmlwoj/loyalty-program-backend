dd85da521d276206d186ac4fb5d00a1237e8bc22
putii <patrykol1964@gmail.com>
2024-05-29 18:51:19 +0200

    contact request endpoints and model

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -3,6 +3,7 @@ using lojalBackend.DbContexts.MainContext;
 using lojalBackend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+
 // using Microsoft.EntityFrameworkCore;
 
 
@@ -31,7 +32,7 @@ namespace lojalBackend.Controllers
         /// Retrieves list of every CONTACT_INFO in the system
         /// </summary>
         /// <returns>List with every CONTACT_INFO</returns>
-        [AllowAnonymous]
+        [AllowAnonymous] // todo: what proper role and policy ?
         [HttpGet("GetContacts")]
         public async Task<IActionResult> GetContacts()
         {
@@ -52,7 +53,7 @@ namespace lojalBackend.Controllers
         /// <param name="contact">New contact</param>
         /// <response code="200">Contact added successfully</response>
         /// <response code="400">Bad request</response>
-        [Authorize(Policy = "IsLoggedIn", Roles = "Administrator")]
+        [Authorize(Policy = "IsLoggedIn", Roles = "Administrator")] // todo: what proper role and policy ?
         [HttpPost("AddContact")]
         public async Task<IActionResult> AddContact([FromBody] ContactInfoModel contact)
         {
@@ -89,7 +90,7 @@ namespace lojalBackend.Controllers
         /// <response code="200">Contact deleted successfully</response>
         /// <response code="400">Bad request</response>
         /// <response code="404">Contact not found</response>
-        [Authorize(Policy = "IsLoggedIn", Roles = "Administrator")]
+        [Authorize(Policy = "IsLoggedIn", Roles = "Administrator")] // todo: what proper role and policy ?
         [HttpDelete("DeleteContact")]
         public async Task<IActionResult> DeleteContact([FromBody] int id)
         {
@@ -99,7 +100,8 @@ namespace lojalBackend.Controllers
             if (contactInfoToDelete == null)
                 return NotFound($"Contact with id {id} was not found in the system.");
 
-            using (var clientTransaction = await clientDbContext.Database.BeginTransactionAsync())
+            using (var clientTransaction =
+                   await clientDbContext.Database.BeginTransactionAsync())
             {
                 try
                 {
@@ -115,5 +117,96 @@ namespace lojalBackend.Controllers
                 }
             }
         }
+
+        /// <summary>
+        /// Adds new contact request to the system
+        /// </summary>
+        /// <returns>Success message</returns>
+        /// <response code="200">Contact request added successfully</response>
+        /// <response code="400">Bad request</response>
+        [AllowAnonymous]
+        [HttpPost("AddContactRequest")]
+        public async Task<IActionResult> AddContactRequest(
+            [FromBody] ContactRequestModel contactRequest)
+        {
+            using (var clientTransaction =
+                   await clientDbContext.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    ContactRequest newDbContextsContactRequest = new()
+                    {
+                        ContReqDate = contactRequest.ContReqDate,
+                        Subject = contactRequest.Subject,
+                        Body = contactRequest.Body
+                    };
+                    await clientDbContext.ContactRequests.AddAsync(newDbContextsContactRequest);
+                    await clientDbContext.SaveChangesAsync();
+                    await clientTransaction.CommitAsync();
+                    return Ok("Contact request added successfully!");
+                }
+                catch (Exception e)
+                {
+                    await clientTransaction.RollbackAsync();
+                    return BadRequest(e.Message); // returns 400 Bad Request response
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retrieves list of every CONTACT_REQUEST in the system
+        /// </summary>
+        /// <returns>List with every CONTACT_REQUEST</returns>
+        /// <response code="200">List of CONTACT_REQUESTs</response>
+        /// <response code="400">Bad request</response>
+        [Authorize(Policy = "IsLoggedIn", Roles = "Administrator")] // todo: what proper role and policy ?
+        [HttpGet("GetAllContactRequests")]
+        public async Task<IActionResult> GetAllContactRequests()
+        {
+            List<ContactRequestModel> answer = new();
+            await foreach (var entity in clientDbContext.ContactRequests.AsAsyncEnumerable())
+            {
+                answer.Add(new ContactRequestModel(entity));
+            }
+
+            return new JsonResult(answer);
+        }
+
+        /// <summary>
+        /// Deletes contact from the system
+        /// </summary>
+        /// <param name="id">id of contact request to delete</param>
+        /// <returns>Sucess message</returns>
+        /// <response code="200">Contact request deleted successfully</response>
+        /// <response code="400">Bad request</response>
+        /// <response code="404">Contact request not found</response>
+        [Authorize(Policy = "IsLoggedIn", Roles = "Administrator")] // todo: what proper role and policy ?
+        [HttpDelete("DeleteContactRequest")]
+        public async Task<IActionResult> DeleteContactRequest([FromBody] int id)
+        {
+            var contactRequestToDelete =
+                await clientDbContext.ContactRequests.FindAsync(id);
+
+            if (contactRequestToDelete == null)
+                return NotFound($"Contact request with id {id} was not found in the system.");
+
+            using (var clientTransaction =
+                   await clientDbContext.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    clientDbContext.ContactRequests.Remove(contactRequestToDelete);
+                    await clientDbContext.SaveChangesAsync();
+                    await clientTransaction.CommitAsync();
+                    return Ok($"Contact request with id {id} was found and deleted from the  system.");
+                }
+                catch (Exception e)
+                {
+                    await clientTransaction.RollbackAsync();
+                    return BadRequest(e.Message);
+                }
+            }
+        }
+
     }
 }
---
+++ Models/ContactRequestModel.cs
@@ -0,0 +1,53 @@
+﻿using lojalBackend.DbContexts.MainContext;
+
+namespace lojalBackend.Models
+{
+    public class ContactRequestModel
+    {
+        public int ContReqId { get; set; } // NOT NULL
+        public DateTime ContReqDate { get; set; } // NOT NULL
+        public string Subject { get; set; } // NOT NULL
+        public string Body { get; set; } // NOT NULL
+
+        public ContactRequestModel()
+        {
+            Subject = string.Empty;
+            Body = string.Empty;
+        }
+
+        public ContactRequestModel(string subject, string body)
+        {
+            ContReqDate = DateTime.Now; // Initialize with the current date/time
+            Subject = subject;
+            Body = body;
+        }
+
+        public ContactRequestModel(int contReqId, DateTime contReqDate, string subject, string body)
+        {
+            ContReqId = contReqId;
+            ContReqDate = contReqDate;
+            Subject = subject;
+            Body = body;
+        }
+
+        public ContactRequestModel(ContactRequest entity)
+        {
+            ContReqId = entity.ContReqId;
+            ContReqDate = entity.ContReqDate;
+            Subject = entity.Subject;
+            Body = entity.Body;
+        }
+
+        public static ContactRequestModel FromEntity(ContactRequest entity)
+        {
+            return new ContactRequestModel
+            {
+                ContReqId = entity.ContReqId,
+                ContReqDate = entity.ContReqDate,
+                Subject = entity.Subject,
+                Body = entity.Body
+            };
+        }
+
+    }
+}
